aa6dbfbe21827d2d2c2b4333c7aee8d5d5c5d461
johnywkr <johnywalker696@gmail.com>
2021-09-09 14:20:39 -0300

    js
    segunda tentativa com js

---
+++ aula51/main.js
@@ -0,0 +1,26 @@
+console.log("hello")
+
+//var nome da variavel - 'valor que eu quero armazenar'
+
+var nome = 'johne'
+console.log(nome)
+
+var idade = 25
+console.log(idade)
+
+var aluno = true
+console.log(aluno, 'debugando a variavel aluno')
+
+var linguagem = ['php', 'javascript', 'sql']
+console.log(linguagem)
+
+var pessoa = {
+    nome:"johne",
+    idade: 25,
+    aluno: true,
+    linguagem: ['php', 'javascript', 'sql']
+
+
+}
+
+console.log(pessoa)
