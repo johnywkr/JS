badff63bd4f2c61929cce9086c74eeee473e7301
johnywkr <johnywalker696@gmail.com>
2021-09-09 14:19:19 -0300

    js
    primeiro passos com js

---
+++ aula48/eu.js
@@ -0,0 +1,28 @@
+console.log("johne");
+
+alert("hello word");
+
+
+
+
+//isso e um comentario 
+
+//string = e tudo que estar entre "aspas"
+
+//interger int
+//1|2|10|12334567
+console.log("31")
+
+//boolean = true | false 
+console.log(true)
+
+
+//array =["hello word",1 , false
+console.log(["string",1,2, "outrastring", true])
+
+//object = {name: "rodrigo"; idade:31}
+console.log({name: 'rodrigo', idade: 31})
+
+
+//FUNCTIONS
+
